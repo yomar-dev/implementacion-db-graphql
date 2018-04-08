44172e5f834e14844325de6495f100a89a2aba30
Yomar Miranda <mirandayomar@gmail.com>
2018-04-08 09:49:44 -0500

    Initial commit.

---
+++ schema.js
@@ -0,0 +1,91 @@
+const { makeExecutableSchema, addMockFunctionsToSchema } = require('graphql-tools')
+const casual = require('casual')
+
+const typeDefs = `
+  # Esto es un curso en el sistema
+  type Curso {
+    id: ID!
+    titulo: String!
+    # Esta es la descripción del curso
+    descripcion: String!
+    profesor: Profesor
+    rating: Float
+    comentarios: [Comentario]
+  }
+
+  type Profesor {
+    id: ID!
+    nombre: String!
+    nacionalidad: String!
+    genero: Genero
+    cursos: [Curso]
+  }
+
+  enum Genero {
+    MASCULINO
+    FEMENINO
+  }
+
+  type Comentario {
+    id: ID!
+    nombre: String!
+    cuerpo: String!
+  }
+
+  type Query {
+    cursos: [Curso]
+    profesores: [Profesor]
+    curso(id: Int): Curso
+    profesor(id: Int): Profesor
+  }
+`
+
+const resolvers = {
+  Query: {
+    cursos: () => {
+      return [{
+        id: 1,
+        titulo: 'Curso de GraphQL',
+        descripcion: 'Aprendiendo GraphQL'
+      }, {
+        id: 2,
+        titulo: 'Curso de PHP',
+        descripcion: 'Aprendiendo PHP'
+      }]
+    }
+  },
+  Curso: {
+    profesor: () => {
+      return {
+        nombre: 'Pablo'
+      }
+    }
+  }
+}
+
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers
+})
+
+addMockFunctionsToSchema({
+  schema,
+  mocks: {
+    Curso: () => {
+      return {
+        id: casual.uuid,
+        titulo: casual.sentence,
+        descripcion: casual.sentences(2)
+      }
+    },
+    Profesor: () => {
+      return {
+        nombre: casual.name,
+        nacionalidad: casual.country
+      }
+    }
+  },
+  preserveResolvers: true
+})
+
+module.exports = schema
