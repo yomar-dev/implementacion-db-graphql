4515ba24212d9bdcb09143da9994742149ab0c4a
Yomar Miranda <mirandayomar@gmail.com>
2018-04-11 22:21:41 -0500

    Creamos la Mutation que nos permite insertar un nuevo registro en la BD.

--- resolvers.js
+++ resolvers.js
@@ -8,6 +8,19 @@ const resolvers = {
     curso: (rootValue, args) => Curso.query().eager('[profesor, comentarios]').findById(args.id),
     profesor: (rootValue, args) => Profesor.query().eager('cursos').findById(args.id)
   },
+  Mutation: {
+    /**
+     * Devuelve el nuevo Profesor creado
+     * Como no vamos a utilizar el Root Value lo vamos a remplazar
+     * por "_" y esto se suele hacer por convención.
+     */
+    profesorAdd: (_, args) => {
+      /**
+       * Insertamos el nuevo profesor en la BD y luego lo retornamos. 
+       */
+      return Profesor.query().insert(args.profesor)
+    }
+  }
 }
 
 module.exports = resolvers
